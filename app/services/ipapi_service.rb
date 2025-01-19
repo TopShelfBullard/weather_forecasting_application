1f48b2742815fe8adbaae81d85a0984efd22dbde
TopShelfBullard <bullardmitchell@gmail.com>
2025-01-19 15:11:39 -0600

    Add error handling to ipapi service.

--- app/services/ipapi_service.rb
+++ app/services/ipapi_service.rb
@@ -8,7 +8,7 @@ class IpapiService
 
   def fetch_location
     response = self.class.get("/#{@ip}/json/")
-    return nil unless response.success?
+    return nil if error_response?(response)
 
     {
       title: get_title_from_response(response),
@@ -20,5 +20,9 @@ class IpapiService
 
   def get_title_from_response(response)
     "#{response["postal"]}: #{response["city"]}, #{response["region"]}, #{response["country_name"]}"
+  end
+
+  def error_response?(response)
+    response["error"] || !response.success?
   end
 end
